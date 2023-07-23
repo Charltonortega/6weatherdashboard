cc159dd27484ea2350fd5bc33bf5087cbc0cf445
Charlton Ortega <charltonortega14@gmail.com>
2023-07-23 19:20:36 +0930

    feat: icons now dynamically change on weather conditon, keypress for enter works

--- assets/script.js
+++ assets/script.js
@@ -3,87 +3,54 @@ var apiUrl ="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 // Function to fetch current weather data for a given city
 function getWeatherData(cityName) { 
-    // Prepare the URL for the API request, include the city name and your API key
-    // Change metric to celsius
     var apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
-    // Use jQuery's ajax function to send a GET request to the API
     $.ajax({
         url: apiUrl,
         method: "GET" 
     }).then(function(response) {
-        // When the response comes back, display the current weather
+        console.log(response.weather);
         displayCurrentWeather(response);
     });
 }
 
-// Function to fetch forecast weather data for a given city
-function getForecastData(cityName) {
-    var apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+// Function to display current weather data
+function displayCurrentWeather(data) {
+    var description = data.weather[0].main;
+    var iconPath = `./assets/images/weather-icons-master/animation-ready/${description.toLowerCase()}.svg`;
+    $("#wicon").attr("src", iconPath);
+    
+    // Update the HTML elements with the new data
+    $("#temperature-display").text(`${Math.round(data.main.temp)}°`);
+    $("#city-display").text(data.name);
+    $("#description-display").text(description);
+    $("#humidity-display").text(`${data.main.humidity}%`);
+    $("#wind-display").text(`${data.wind.speed} km/h`);
 
-    $.ajax({
-        url: apiUrl,
-        method: "GET"
-    });
+    // Update the date and time every second
+    setInterval(function() {
+        $("#date-display").text(getCurrentDay());
+        $("#time-display").text(getCurrentTime());
+    }, 1000); 
 }
 
 // Function to get current time
 function getCurrentTime() {
-    var currentDate = new Date();
-    var time = currentDate.toLocaleTimeString();
-    return time;
+    return new Date().toLocaleTimeString();
 }
 
 // Function to get current day
 function getCurrentDay() {
-    var currentDate = new Date();
-    var day = currentDate.toLocaleDateString();
-    return day;
+    return new Date().toLocaleDateString();
 }
-
-// Function to display current weather data
-function displayCurrentWeather(data) {
-    // Find the elements in the HTML where we want to display data
-    var temperatureElement = $("#temperature-display");
-    var cityNameElement = $("#city-display");
-    var dateElement = $("#date-display");
-    var timeElement = $("#time-display");
-    var descriptionElement = $("#description-display");
-
-    // Get the data we want to display
-    var temperature = `${Math.round(data.main.temp)}°`; // round of nearest number 
-    var cityName = data.name; // city name
-    var description = data.weather[0].main; // weather description
-
-    // Update the HTML elements with the new data
-    temperatureElement.text(temperature);
-    cityNameElement.text(cityName);
-    descriptionElement.text(description);
-
-    // Repeat the same steps for humidity, sun-set time, and wind speed
-    var humidityElement = $("#humidity-display");
-    var windSpeedElement = $("#wind-display");
-
-    var humidity = `${data.main.humidity}%`;
-    var windSpeed = `${data.wind.speed} km/h`;
-
-    humidityElement.text(humidity); // Update the humidity
-    windSpeedElement.text(windSpeed); // Update the wind speed
-
-    // Update the date and time every second
-    setInterval(function() {
-        var date = getCurrentDay(); // current date
-        var time = getCurrentTime(); // current time
-        dateElement.text(date);
-        timeElement.text(time);
-    }, 1000); // 1000 ms = 1 second
-}
+// Functionaility for keypress on "enter" search
+$("#search").keypress(function(e) {
+    if (e.which == 13) {  // 13 is the code for the Enter key
+        $("button").click();
+    }
+});
 
 // Function to handle the click event on the search button
 $("button").click(function() {
-    // Get the city name from the input field
-    var cityName = $("#search").val().trim(); // Get the city name from the input field, and trim the whitespace from the beginning and end
-
-    // Fetch weather data for the city
-    getWeatherData(cityName);
+    getWeatherData($("#search").val().trim());
 });
